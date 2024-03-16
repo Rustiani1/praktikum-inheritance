d4b6bd70d34b9c605ccba8b808c201cda51ddbb2
Rustiani1 <123487139+Rustiani1@users.noreply.github.com>
2024-03-16 23:43:50 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,26 @@
+//Rustiani_20220040003_TI22C_PBO3
+
+public class Account {
+    protected double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance - amount >= 0) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ SavingAccount.java
@@ -0,0 +1,5 @@
+public class SavingAccount extends Account {
+    public SavingAccount(double balance, double interest_rate) {
+        super(balance);
+    }
+}
